316ae755493169cc0ee40b72b6c49140bae14e78
Adrien Courtois <adrien.courtois@telecom-paristech.fr>
2018-12-29 16:56:24 +0100

    added docs & fixed compatibility with research mode

--- script.js
+++ script.js
@@ -19,12 +19,20 @@
 (function($){
 	var paginationElements = [];
 
+	/**
+	 * Search functionnality:
+	 * Bind the "search" and "unsearch" event in order to only display the searched elements.
+	 * 
+	 * The "format" parameter is a function transforming an HTML element to a text element.
+	 * When called, the "search" event is passed a string which is matched with the text of each HTML element.
+	 * 
+	 * The "unsearch" event cancels the search mode. It is for example called when the search query is empty.
+	 */
 	$.fn.search = function(options) {
 		var settings = $.extend({
 			format: (elt) => $(elt).html()
 		}, options);
 
-		var self = this;
 		var index = parseInt($(this).attr('data-pagination-index'));
 
 		if (isNaN(index)) {
@@ -56,6 +64,11 @@
 		});
 	};
 
+	/**
+	 * Pagination functionnality:
+	 * Put the given element to be paged in a queue and displays a pager for it.
+	 * The pager is then binded with the click events related.
+	 */
 	$.fn.pagination = function(options) {
 		if (options == null)
 			options = {};
@@ -83,25 +96,26 @@
 		
 		if (paginationElements[index].pager != null)
 			$(paginationElements[index].pager).remove();
-			
+
+
 		// managing the pager
 		if (nbPage > 1){
-			var pager_html = '<nav class="pagination-pager" style="text-align: center;" data-total-page="' + nbPage + '" data-current-page="1">'
-			+ '<ul class="pagination">'
-			+ '<li class="page-item disabled nav-link-previous unbinded"><a class="page-link" href="#">Previous</a></li>'
-			+ '<li class="page-item active unbinded"><a class="page-link" href="#">1</a></li>'
-			+ '<li class="page-item page-dots" style="display: none;"><span class="page-link">...</span></li>'
-			+ '<li class="page-item active page-movable" style="display: none;"><a class="page-link" href="#"></a></li>'
-			+ '<li class="page-item page-dots" ' + ((nbPage > 2) ? '' : 'style="display: none;"') + '><span class="page-link">...</span></li>'
-			+ '<li class="page-item unbinded"><a class="page-link" href="#">' + nbPage + '</a></li>'
-			+ '<li class="page-item nav-link-next unbinded"><a class="page-link" href="#">Next</a></li>'
-			+ '</ul></nav>';
+			var pager_html = `<nav class="pagination-pager" style="text-align: center;" data-total-page="${nbPage}" data-current-page="1">
+			<ul class="pagination">
+				<li class="page-item disabled nav-link-previous unbinded"><a class="page-link" href="#">Previous</a></li>
+				<li class="page-item active unbinded"><a class="page-link" href="#">1</a></li>
+				<li class="page-item page-dots" style="display: none;"><span class="page-link">...</span></li>
+				<li class="page-item active page-movable" style="display: none;"><a class="page-link" href="#"></a></li>
+				<li class="page-item page-dots" ${((nbPage > 2) ? '' : 'style="display: none;"')}><span class="page-link">...</span></li>
+				<li class="page-item unbinded"><a class="page-link" href="#">${nbPage}</a></li>
+				<li class="page-item nav-link-next unbinded"><a class="page-link" href="#">Next</a></li>
+			</ul></nav>`;
 
 			$(settings.container).append(pager_html);
 			var pager = $(settings.container).find('.pagination-pager');
 			paginationElements[index].pager = pager;
 
-			// bind events
+			// bind events related to the pager
 			pager.find('.nav-link-previous.unbinded').click(function(){
 				$(self).paginationPreviousPage();
 				return false;
@@ -147,8 +161,12 @@
 		var firstElementIndex = (nb - 1) * self.settings.nbPerPage;
 		$(self.settings.elements).css('display', 'none');
 		
-		for (var i = 0 ; i < self.settings.nbPerPage ; i++)
-			$(self.settings.elements).eq(firstElementIndex + i).css('display', self.settings.display);
+		for (var i = 0 ; i < self.settings.nbPerPage ; i++){
+			if (!self.searchMode)
+				$(self.settings.elements).eq(firstElementIndex + i).css('display', self.settings.display);
+			else
+				$(self.settings.elements).filter('.search-mode').eq(firstElementIndex + i).css('display', self.settings.display);
+		}
 		
 		// pager display
 		if (nb == 1) {
@@ -183,8 +201,7 @@
 		var current_page = parseInt($(pager).attr('data-current-page'));
 		var total_page = parseInt($(pager).attr('data-total-page'));
 
-		if (current_page == total_page)
-			return;
+		if (current_page == total_page) return;
 
 		$(this).paginationChangePage(current_page + 1);
 
@@ -195,9 +212,8 @@
 		var pager = paginationElements[parseInt($(this).attr('data-pagination-index'))].pager;
 		var current_page = parseInt($(pager).attr('data-current-page'));
 
-		if (current_page == 1)
-			return;
-		
+		if (current_page == 1) return;
+
 		$(this).paginationChangePage(current_page - 1);
 
 		return this;
